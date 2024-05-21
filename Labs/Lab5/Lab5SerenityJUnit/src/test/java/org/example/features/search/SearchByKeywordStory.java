a785c962308ca7ad8af54f2699000d18a113ac75
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2024-05-21 19:57:18 +0300

    Added DDT

--- Labs/Lab5/Lab5SerenityJUnit/src/test/java/org/example/features/search/SearchByKeywordStory.java
+++ Labs/Lab5/Lab5SerenityJUnit/src/test/java/org/example/features/search/SearchByKeywordStory.java
@@ -21,22 +21,6 @@ public class SearchByKeywordStory {
     @Steps
     public EndUserSteps anna;
 
-    @Issue("#WIKI-1")
-    @Test
-    public void searching_by_keyword_apple_should_display_the_corresponding_article() {
-        anna.is_the_home_page();
-        anna.looks_for("apple");
-        anna.should_see_definition("A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.");
-
-    }
-
-    @Test
-    public void searching_by_keyword_banana_should_display_the_corresponding_article() {
-        anna.is_the_home_page();
-        anna.looks_for("pear");
-        anna.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
-    }
-
     @Pending @Test
     public void searching_by_ambiguous_keyword_should_display_the_disambiguation_page() {
     }
@@ -46,5 +30,12 @@ public class SearchByKeywordStory {
         anna.is_the_home_page();
         anna.looks_for("football");
         anna.should_see_definition("A sport played on foot in which teams attempt to get a ball into a goal or zone defended by the other team.");
+    }
+
+    @Test
+    public void searching_by_keyword_bistrita_should_Display_the_corresponding_article(){
+        anna.is_the_home_page();
+        anna.looks_for("Bistrita");
+        anna.should_see_definition("The capital city of Bistrița-Năsăud County, Romania.");
     }
 } 
